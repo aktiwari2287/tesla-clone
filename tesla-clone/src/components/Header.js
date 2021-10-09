1b925f21d233706e3258394abd627518fe6f5af5
Tiwari <AnandKumar.Tiwari@gartner.com>
2021-10-09 18:31:40 +0530

    Added redux

--- tesla-clone/src/components/Header.js
+++ tesla-clone/src/components/Header.js
@@ -2,19 +2,21 @@ import React, {useState} from 'react'
 import styled from "styled-components"
 import MenuIcon from '@material-ui/icons/Menu'
 import CloseIcon from '@material-ui/icons/Close';
+import { selectCars } from "../features/car/carSlice"
+import { useSelector } from 'react-redux'
 function Header() {
     const [burgerStatus, setBurgerStatus] = useState(false);
-   
+    const cars = useSelector(selectCars);
+    console.log(cars);
     return (
         <Container>
            <a>
             <img src="/images/logo.svg" />
            </a>
            <Menu>
-                <a href="#">Model S</a>
-                <a href="#">Model 3</a>
-                <a href="#">Model X</a>
-                <a href="#">Model Y</a>
+                { cars && cars.map((car, index) => (
+                    <a key={index} href="#"> {car} </a>
+                ))}
             </Menu>
            <RightMenu>
             <a href="#">Shop</a>
@@ -25,6 +27,9 @@ function Header() {
                 <CloseWrapper>
                     <CustomClose onClick={()=>setBurgerStatus(false)}/>
                 </CloseWrapper>
+                { cars && cars.map((car, index) => (
+                    <li key={index}> <a href="#"> {car} </a></li>
+                ))}
                 <li><a href="#">Existing Inventory</a></li>
                 <li><a href="#">Used Inventory</a></li>
                 <li><a href="#">Trade-in</a></li>
@@ -90,6 +95,7 @@ const BurgerNav = styled.div`
     flex-direction: column;
     text-align:left;
     transform: ${props => props.show ? 'translateX(0)': 'translateX(100%)'};
+    transition: transform 0.2s;
     li {
         padding: 15px 0;
         border-bottom: 1px solid rgba(0,0,0,.2);
